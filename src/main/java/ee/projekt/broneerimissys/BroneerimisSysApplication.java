2e1ff10bba881a88895fd66416852da5906c7e78
Kristjan Tamm <kluis2206@gmail.com>
2021-12-06 11:07:39 +0200

    Initial commit

---
+++ src/main/java/ee/projekt/broneerimissys/BroneerimisSysApplication.java
@@ -0,0 +1,13 @@
+package ee.projekt.broneerimissys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroneerimisSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroneerimisSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/projekt/broneerimissys/BroneerimisSysApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.projekt.broneerimissys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroneerimisSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
