69cd4a56aab73a6f1d4fc84612aca55d2efddf6a
Kristjan Tamm <kluis2206@gmail.com>
2021-12-07 14:28:53 +0200

    changelog typo ja doc post teenus

--- src/main/java/ee/projekt/broneerimissys/ProjectService.java
+++ src/main/java/ee/projekt/broneerimissys/ProjectService.java
@@ -1,6 +1,7 @@
 package ee.projekt.broneerimissys;
 
 
+import DTOs.Doctor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,8 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    public String createDoc(Doctor doctor) {
+        projectRepository.createDoc(doctor);
+        return "New doctor, with license number " + doctor.getDoc_license() + " has been added to the database.";
+    }
 }
