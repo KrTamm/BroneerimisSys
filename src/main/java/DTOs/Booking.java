05f7c99de98ad120ce43da6463df1d3d2d297091
Miilen <kristianmiilen@gmail.com>
2021-12-07 12:06:56 +0200

    Miilen ver.3 (doctor ja booking DTO ja lisatulp doctoris license)

---
+++ src/main/java/DTOs/Booking.java
@@ -0,0 +1,49 @@
+package DTOs;
+
+public class Booking {
+    private Integer booking_id;
+    private Integer doc_id;
+    private String user_mail;
+    private String booking_date;
+    private String booking_time;
+
+    public Integer getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(Integer booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public Integer getDoc_id() {
+        return doc_id;
+    }
+
+    public void setDoc_id(Integer doc_id) {
+        this.doc_id = doc_id;
+    }
+
+    public String getUser_mail() {
+        return user_mail;
+    }
+
+    public void setUser_mail(String user_mail) {
+        this.user_mail = user_mail;
+    }
+
+    public String getBooking_date() {
+        return booking_date;
+    }
+
+    public void setBooking_date(String booking_date) {
+        this.booking_date = booking_date;
+    }
+
+    public String getBooking_time() {
+        return booking_time;
+    }
+
+    public void setBooking_time(String booking_time) {
+        this.booking_time = booking_time;
+    }
+}
