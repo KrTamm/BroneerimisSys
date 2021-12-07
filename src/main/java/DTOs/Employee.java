915b9a1792af163494df1107ee8b3387aaf8d98b
Kristjan Tamm <kluis2206@gmail.com>
2021-12-07 09:10:32 +0200

    BE projekti baas

---
+++ src/main/java/DTOs/Employee.java
@@ -0,0 +1,49 @@
+package DTOs;
+
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String LastName;
+    private String position;
+    private String country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/ee/projekt/broneerimissys/Project.java
@@ -0,0 +1,47 @@
+package ee.projekt.broneerimissys;
+
+import DTOs.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class Project {
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+
+    @GetMapping("/api/employees")
+    public List<Employee> getEmployeeList(String orderBy) {
+        String sql = "SELECT * FROM employee";
+        HashMap<String, Object> paramMap = new HashMap<>();
+        if (orderBy != null && !orderBy.isEmpty()) {
+            sql += " ORDER BY " + orderBy;
+        }
+        List<Employee> result = jdbcTemplate.query(sql, paramMap, new EmployeeRowMapper());
+        return result;
+    }
+
+    static class EmployeeRowMapper implements RowMapper<Employee> {
+
+        @Override
+        public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
+            Employee result = new Employee();
+            result.setId(resultSet.getInt("id"));
+            result.setFirstName(resultSet.getString("first_name"));
+            result.setLastName(resultSet.getString("last_name"));
+            result.setPosition(resultSet.getString("position"));
+            result.setCountry(resultSet.getString("country"));
+            return result;
+        }
+
+    }
+}
